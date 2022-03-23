e46806e14c1dcc81247908a040c205bca3cafc13
Obi Enechionyia <obienechionyia@gmail.com>
2022-03-23 01:05:35 +0100

    Added geolocation function

--- weather_gui.py
+++ weather_gui.py
@@ -5,12 +5,15 @@ import requests
 from requests import get
 from keys import *
 from tkinter import *
+from tkinter import ttk
+import ipinfo
+import flag
+import requests
+from requests.structures import CaseInsensitiveDict
+
 
 # Key from OpenWeatherMaps API
 apiKey = api_key
-
-# Declare global variables to be used in various functions
-# global currentWeather
 
 url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}"
 
@@ -24,19 +27,33 @@ def getWeather(city):
         temp_c = (temp_f - 32) * .5556
         icon = currentWeather["weather"][0]["icon"]
         weather = currentWeather["weather"][0]["main"]
-        final = (cityName, country, temp_f, temp_c, icon, weather)
+        country_flag = flag.flag(f"{country}")
+        final = (cityName, country, temp_f, temp_c, icon, weather, country_flag)
         return final
     else:
         return None
 
-def search():
-    city = city_text.get()
+def search():  
+    city = city_text.get().lower()
+    if city == "my location":
+        city = getLocation()
     weather = getWeather(city)
     if weather:
-        location_lbl['text'] = "{},{}".format(weather[0], weather[1])
-        temp_lbl['text'] = '{}°'.format(weather[2])
+        location_lbl['text'] = "{}, {}".format(weather[0], weather[1]) + " " + weather[6]
+        temp_lbl['text'] = '{}°F'.format(weather[2])
         image['bitmap'] = "weather_icons/{}.png".format(weather[4])
         weather_lbl['text'] = '{}'.format(weather[5])
+    instructions_lbl.destroy()
+    weather_icon.destroy()
+
+def getLocation():
+    # Get user IP address
+    ipAddress = get('https://api.ipify.org').text
+    # Convert IP address to geolocation data
+    geoKey = geo_key
+    geoHandler = ipinfo.getHandler(geoKey)
+    geoDetails = geoHandler.getDetails(ipAddress)
+    return geoDetails.city
 
 
 app = Tk()
@@ -44,9 +61,15 @@ app = Tk()
 app.title("Weather App")
 app.geometry("700x350")
 
+instructions_lbl = Label(app, text="Enter a city name below to see its current weather report!", font=("bold", 25))
+instructions_lbl.pack()
+
+weather_icon = Label(app, bitmap="weather_icons/weather.png")
+weather_icon.pack()
 
 location_lbl = Label(app, text="", font=("bold", 20))
 location_lbl.pack()
+
 image = Label(app, bitmap="")
 image.pack()
 
@@ -63,4 +86,7 @@ city_entry.pack()
 search_btn = Button(app, text="Search", width=12, command=search)
 search_btn.pack()
 
+
 app.mainloop()
+
+
